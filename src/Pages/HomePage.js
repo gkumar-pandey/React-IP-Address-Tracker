80db74bcba7d73741606e2d28ce323a104be54d4
gkumar-pandey <gautamshekhar078@gmail.com>
2023-02-23 22:48:12 +0530

    feat: Home page created

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Header from "../Components/Header";
 import Map from "../Components/Map";
 
 const HomePage = () => {
+  const getIpAddress = async () => {};
   return (
     <div>
-      <Map />
+      <div className="h-[30vh]">
+        <Header />
+      </div>
+      <div className="border border-black z-[-40] ">
+        <Map />
+      </div>
     </div>
   );
 };
