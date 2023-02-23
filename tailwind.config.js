6da40604e5f9ce4feecaa25b5f06f1c9666e270f
gkumar-pandey <gautamshekhar078@gmail.com>
2023-02-23 22:51:05 +0530

    tailwind initialize

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {}
+    extend: {},
+
+    screens: {
+      sm: { max: "500px" },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      md: { min: "502px", max: "768px" },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: "770px", max: "1024px" },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      xl: { max: "1290px" },
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      "2xl": { min: "1536px" }
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: []
 };
