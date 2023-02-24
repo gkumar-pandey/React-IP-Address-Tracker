33c6dde59fd2d0e9da86cdea6351cccf47fc7d0c
gkumar-pandey <gautamshekhar078@gmail.com>
2023-02-24 14:01:24 +0530

    deleted: delete some img

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -1,6 +1,5 @@
 import bgImgDesktop from "./images/pattern-bg-desktop.png";
 import locationIcon from "./images/icon-location.svg";
 import arrowIcon from "./images/icon-arrow.svg";
-import bgImgMobile from "./images/pattern-bg-mobile.png";
 
-export { bgImgDesktop, locationIcon, arrowIcon, bgImgMobile };
+export { bgImgDesktop, locationIcon, arrowIcon };
